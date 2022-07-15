2f40b7d2bdc87bca8158889cbac303fb19581221
Nyr <nyrdeveloper@gmail.com>
2022-07-15 14:08:07 +0200

    reducing distance checking at max. to allow smaller gaps if Divine Star is cast really close to walls

--- src/server/scripts/Spells/spell_priest.cpp
+++ src/server/scripts/Spells/spell_priest.cpp
@@ -404,7 +404,7 @@ struct areatrigger_pri_divine_star : AreaTriggerAI
         if (!caster)
             return;
 
-        if (at->GetDistance(_casterCurrentPosition) > 1.0f)
+        if (at->GetDistance(_casterCurrentPosition) > 0.05f)
         {
             _affectedUnits.clear();
 
