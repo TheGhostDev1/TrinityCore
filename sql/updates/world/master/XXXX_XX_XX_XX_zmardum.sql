db10a15abc66258671c682da51c2ed8cdab9581b
Naddley <NaddleyTC@gmail.com>
2023-08-29 20:34:42 +0200

    Update SQL

--- sql/updates/world/master/XXXX_XX_XX_XX_zmardum.sql
+++ sql/updates/world/master/XXXX_XX_XX_XX_zmardum.sql
@@ -72,7 +72,7 @@ SET @ENTRY := 94580;
 UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `entry` = @ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryOrGuid` = @ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
-(@ENTRY, 0, 0, 0, 63, 0, 100, 0, 0, 0, 0, 0, 53, 1, 7566400, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'On just created - Self: Start path #7566400, run, do not repeat, Passive');
+(@ENTRY, 0, 0, 0, 63, 0, 100, 0, 0, 0, 0, 0, 53, 1, 9458000, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'On just created - Self: Start path #9458000, run, do not repeat, Passive');
 
 -- Waypoints for Illidari
 SET @ENTRY := 98228; -- Jayce Darkweaver
